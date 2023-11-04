e4fda0556a971b15745aaa016c7a98897a339d9b
Carlos Sandoval <carlosmgsv22@gmail.com>
2023-11-03 21:55:22 -0600

    initial php fies

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forms</title>
+</head>
+<body>
+    
+    <p>Forms in php</p>
+    <form action="includes/formHandler.php" method="post">
+        <label for="firstname">First name: </label><br>
+        <input type="text" id="firstname" name="firstname"><br>
+        <label for="secondname">Second name: </label><br>
+        <input type="text" id="secondname" secondName="secondname"><br>
+        <label for="favoritemascot">Favorite mascot: </label><br>
+        <input type> 
+    </form>
+
+</body>
+</html>
